499e0988ee4b389b04bea9b1e76f2e7356e2cc27
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-12-30 22:32:50 +0530

    imports in home component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,14 @@
-//this is the main page of the application
+import {motion,AnimatePresence} from "framer-motion"
+import { useSnapshot } from "valtio"
 
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from "../config/motion"
+
+//this is the main page of the application
 //called from App.jsx
 const Home = () => {
   return (
